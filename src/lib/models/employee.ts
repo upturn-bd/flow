c59e43d800b7a34460b70b84a64882ab78f20080
Saim20 <saim.29@outlook.com>
2025-02-17 23:00:28 +0600

    Employee interface updates

--- src/lib/models/employee.ts
+++ src/lib/models/employee.ts
@@ -1,18 +1,51 @@
-export interface Employee {
+export interface EmployeeBasicInfo {
     id: string;
     firstName: string;
     lastName: string;
     email: string;
-    supervisorName: string;
+    supervisorName?: string;
     username: string;
     role: 'end user' | 'supervisor' | 'HR' | 'system admin' | 'super admin';
-    lastLogin: Date;
-    isActive: boolean;
-    subordinates: string[];
+    lastLogin?: Date;
+    isActive?: boolean;
+    subordinates?: string[];
+    companyName?: string;
+    designation?: string;
+    department?: string;
+    phone?: string;
+    jobStatus?: string;
+    joiningDate?: Date;
+}
+
+export interface EmployeePersonalInfo {
+    gender?: string;
+    fatherName?: string;
+    dateOfBirth?: Date;
+    motherName?: string;
+    religion?: string;
+    spouseName?: string;
+    bloodGroup?: string;
+    emergencyContact?: string;
+    maritalStatus?: string;
+    relationWithEC?: string;
+    children?: string[];
+    phoneEC?: string;
+    nidNumber?: number;
+    address?: string;
+}
+
+export interface EmployeeEducationInfo {
+    degree: string;
+    institution: string;
+    from: Date;
+    to?: Date;
+    result?: string;
+}
+
+export interface EmployeeExperienceInfo {
     companyName: string;
     designation: string;
-    department: string;
-    phone: string;
-    jobStatus: string;
-    joiningDate: Date;
+    description?: string;
+    from: Date;
+    to?: Date;
 }
