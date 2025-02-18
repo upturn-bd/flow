0793857daa4b3b020e64addc6523ba2bd2eb5e49
Saim20 <saim.29@outlook.com>
2025-02-18 16:46:27 +0600

    Implemented google sign in

--- src/app/(auth)/_actions.ts
+++ src/app/(auth)/_actions.ts
@@ -64,7 +64,7 @@ export async function googleSignIn(){
   const response = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options:{
-      redirectTo: "http://localhost:3000/",
+      redirectTo: "http://localhost:3000/auth/callback",
     }
   });
 
