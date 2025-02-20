6af9c7dbffb57877feda06cb9a1312aa1baba745
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2025-02-20 23:17:54 +0600

    reports page added

---
+++ src/app/(home)/reports/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='container mx-auto '>
+        <p>Individual Performance Statement</p>
+        <p>Stakeholders’ Invoice</p>
+        <p>Salary Statement</p>
+        <p>Experience Certificate</p>
+        <p>Performance Appraisal Certificate (Director’s Award type)</p>
+
+    </div>
+  )
+}
