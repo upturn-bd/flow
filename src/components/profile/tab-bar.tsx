366e2efc804b7075963ad63f9896568a6c4a3b1d
MahirMahdi <mahdimahir45@gmail.com>
2025-04-25 11:37:01 +0600

    modified profile nav tab bar

--- src/components/profile/tab-bar.tsx
+++ src/components/profile/tab-bar.tsx
@@ -1,66 +1,40 @@
-'use client';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-import clsx from 'clsx';
-import Link from 'next/link';
-import { usePathname, useSearchParams } from 'next/navigation';
-
-
-interface NavItem {
-  label: string;
-  href: string;
-}
-
-const navItems: NavItem[] = [
-  {
-    label: 'Basic Information',
-    href: '/profile',
-
-  },
-  {
-    label: 'Personal Information',
-    href: '/profile/personal-info',
-  },
-  {
-    label: 'Education & Experience',
-    href: '/profile/experience',
-  },
-  {
-    label: 'Key Performance Indicator',
-    href: '/profile/kpi',
-  },
-  {
-    label: 'Performance Evaluation',
-    href: '/profile/evaluation',
-  },
-];
-
-export default function TabView(
-) {
+export function ProfileTabs() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const uid = searchParams.get('uid');
-
   return (
-    <div className={clsx(
-      "flex items-center mx-20 m-[45px] border border-gray-200 rounded-lg",
-      {
-        'shadow-md': pathname === '/profile',
-      }
-    )}>
-      {navItems.map((link, index) => (
-        <Link
-          key={index}
-          href={link.href + (uid?.length ?? 0 > 0 ? `?uid=${uid}` : '')}
-          className={clsx(
-            'w-70 px-5 text-2xl py-5 first:rounded-l-lg border-r last:border-r-0 border-black last:rounded-r-lg h-full flex items-center justify-center text-center transition-colors duration-300',
-            {
-              'bg-white shadow-md text-blue-900 font-semibold': pathname === link.href,
-            },
-          )}
-        >
-          {link.label}
-        </Link>
-      ))}
+    <div className="flex items-center justify-between border-b border-gray-200 bg-gray-300 rounded-lg mb-12">
+      <Link
+        href="/profile"
+        className={`w-full px-4 py-6 font-medium text-center border-r-2 border-gray-800 ${
+          pathname === `/profile`
+            ? "font-bold text-blue-700"
+            : "text-gray-700 hover:text-blue-600"
+        }`}
+      >
+        Basic Information
+      </Link>
+      <Link
+        href="/profile/personal-info"
+        className={`w-full px-4 py-6 font-medium text-center border-r-2 border-gray-800 ${
+          pathname === `/profile/personal-info`
+            ? "font-bold text-blue-700"
+            : "text-gray-700 hover:text-blue-600"
+        }`}
+      >
+        Personal Information
+      </Link>
+      <Link
+        href="/profile/education-and-experience"
+        className={`w-full px-4 py-6 font-medium text-center ${
+          pathname === `/profile/education-and-experience`
+            ? "font-bold text-blue-700"
+            : "text-gray-700 hover:text-blue-600"
+        }`}
+      >
+        Education & Experience
+      </Link>
     </div>
   );
-};
+}
