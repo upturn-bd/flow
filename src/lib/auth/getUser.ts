7973dc0963c46ce4355281927b23613821b83364
MahirMahdi <mahdimahir45@gmail.com>
2025-05-09 11:58:04 +0600

    added get company id function

--- src/lib/auth/getUser.ts
+++ src/lib/auth/getUser.ts
@@ -1,16 +1,55 @@
-"use server";
+"use server"
 
 import { createClient } from "../supabase/server";
 import { User } from "@supabase/supabase-js";
 
-export async function getUser(): Promise<{ user: User | null; }> {
-    const supabase = await createClient();
-    const { data, error } = await supabase.auth.getUser();
+export async function getUser(): Promise<{ user: User | null }> {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
 
-    if (data.user) {
-        return { user: data.user };
-    }
-    if (error)
-        console.error(error);
-    return { user: null }; // or fetch and return the actual EmployeeBasicInfo
+  if (data.user) {
+    return { user: data.user };
+  }
+  if (error) console.error(error);
+  return { user: null };
+}
+
+export async function getEmployeeId() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+    error: authError,
+  } = await supabase.auth.getUser();
+
+  if (authError || !user) {
+    return { error: "Not authenticated" };
+  }
+
+  return user.id;
+}
+
+export async function getCompanyId() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+    error: authError,
+  } = await supabase.auth.getUser();
+
+  if (authError || !user) {
+    return { error: "Not authenticated" };
+  }
+
+  const { data: employee, error: employeeError } = await supabase
+    .from("employees")
+    .select("company_id")
+    .eq("id", user.id)
+    .single();
+
+  if (employeeError) {
+    return { error: "Error fetching employee data" };
+  }
+
+  return employee.company_id;
 }
